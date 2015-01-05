27c0a970c15756fe123a93ec18008b3e971d96db
moslifulong <msl.fulong@gmail.com>
2015-01-05 17:32:57 +0800

    add todo.md to project @lifulong

---
+++ TODO.md
@@ -0,0 +1,10 @@
+MapMatch TODO
+=============
+
+*	json | yaml config file support
+*	local and network logger support
+*	ST-Matching, HMM model, ... support
+*	map-index, map-matching service(use ace) support
+*	auto test support
+
+
--- src/map-index/serve.cpp
+++ src/map-index/serve.cpp
@@ -1,7 +1,64 @@
+/**
+ *
+ * Use Ace.
+ *
+ *
+ *
+ ***/
+
 #include "map_index.h"
+
+#ifdef TEST_MAP_INDEX
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <stdio.h>
+
+void test()
+{
+	vector<seg_point_map> maps;
+	maps.clear();
+
+	MapIndex *map_index = new MapIndex("", "", "", "segs.txt", "grid_info", "grid.txt");
+	printf("begin query grid segs......\n");
+	maps = map_index->getGridSegs(115.714691, 39.570098, 200);
+
+	printf("maps_size: %d\n", maps.size());
+	for(vector<seg_point_map>::iterator iter = maps.begin(); iter != maps.end(); iter++)
+	{
+		printf("Map:\n");
+		printf("seg_id:\t%d\n", iter->seg_id);
+		printf("edge_id:\t%d\n", iter->edge_id);
+		printf("start_lng:\t%f\n", iter->start_lng);
+		printf("start_lat:\t%f\n", iter->start_lat);
+		printf("end_lng:\t%f\n", iter->end_lng);
+		printf("end_lat:\t%f\n", iter->end_lat);
+		printf("lng:\t%f\n", iter->lng);
+		printf("lat:\t%f\n", iter->lat);
+		printf("map_lng:\t%f\n", iter->map_lng);
+		printf("map_lat:\t%f\n", iter->map_lat);
+		printf("distance:\t%f\n", iter->distance);
+		printf("min_distance:\t%f\n", iter->min_distance);
+		printf("on_seg:\t%d\n", iter->on_seg);
+		printf("\n");
+	}
+}
+
+int main()
+{
+	test();
+	return 0;
+}
+
+#else
 
 int main()
 {
 	return 0;
 }
+
+#endif
 
